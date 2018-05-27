99f6f7eaf87d0db9d6bc6f57dbc44f7602debd9d
zBearchenPlayz <38949581+zBearchenPlayz@users.noreply.github.com>
2018-05-27 18:29:04 +0200

    Update coinsystem.php

--- CoinSystem/src/zBearchenPlayz/coinsystem.php
+++ CoinSystem/src/zBearchenPlayz/coinsystem.php
@@ -1,5 +1,5 @@
 <?php
-namespace zBearchenPlayz\CoinSystem;
+namespace zBearchenPlayz\coinsystem;
 use zBearchenPlayz\Provider\MySQLDataProvider;
 use zBearchenPlayz\API;
 use zBearchenPlayz\Listeners\PlayerJoinListener;
